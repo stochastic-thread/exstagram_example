5c6553e8ae2d7713ed79ec0d9ec8563cb2f2cd16
Arthur Colle <hello@arthurcolle.com>
2015-06-16 03:26:19 -0400

    Heroku deploy

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -5,3 +5,8 @@ use Mix.Config
 # your version control system.
 config :example_exstagram, ExampleExstagram.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :example_exstagram, ExampleExstagram.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool
