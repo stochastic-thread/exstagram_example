8a5acd6b1a76c6247a53848513a3eec6bf595d58
Arthur Colle <hello@arthurcolle.com>
2015-06-16 03:06:58 -0400

    Mistake in config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :heroku_test_app, ExampleExstagram.Endpoint,
+config :example_exstagram, ExampleExstagram.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # config :heroku_test_app, HerokuTestApp.Repo,
